b2a80f44aa68b74a53c68b6ff945049d6799b87a
Anton Alekseev <anton.m.alexeyev@gmail.com>
2023-02-25 06:57:46 +0300

    Hyperparameter optimization

--- param_opt_tomotopy.py
+++ param_opt_tomotopy.py
@@ -7,24 +7,20 @@ from common_reading_utils import get_20ng
 from common_eval_utils import proportion_unique_words
 import matplotlib.pyplot as plt
 
+print("Reading data...")
+
 news_df, word_list_lemmatized = get_20ng()
 
-study = optuna.create_study(direction="maximize", study_name="best_tomotopy_model")
-
-# maybe we should provide a nice starting point
-# study.enqueue_trial({"num_topics": 40, "passes": 50, "chunksize": 100})
-
-print("Optuna study created, let's find those hyperparams!")
+print("Data read.")
 
 diversities, coherences = [], []
 topn_evaluation = 10
 
 
 def objective(trial: optuna.trial.Trial):
-
     # создаём перебираемые гиперпараметры
-    num_topics = trial.suggest_int("num_topics", low=15, high=70)
-    iter = trial.suggest_int("iter", low=5, high=500, step=5)
+    num_topics = trial.suggest_int("num_topics", low=10, high=70)
+    iter = trial.suggest_int("iter", low=5, high=10000, log=True)
     alpha = trial.suggest_float("alpha", low=0.001, high=3, log=True)
     eta = trial.suggest_float("eta", low=0.001, high=3, log=True)
 
@@ -37,7 +33,6 @@ def objective(trial: optuna.trial.Trial):
 
     mdl.train(iter=iter, workers=0, parallel=tp.ParallelScheme.DEFAULT)
     print('Log-likelihood: {}'.format(mdl.ll_per_word))
-    print("Training done. Evaluation starting.")
 
     top_terms = []
 
@@ -70,15 +65,30 @@ def objective(trial: optuna.trial.Trial):
     return average_coherence
 
 
-study.optimize(objective, n_trials=100)
+print("Setting the study up...")
+
+# sampler = optuna.samplers.QMCSampler()
+study = optuna.create_study(direction="maximize", study_name="best_tomotopy_model")#, sampler=sampler)
+
+# maybe we should provide a nice starting point
+# study.enqueue_trial({"num_topics": 20, "iter": 100, "alpha": 0.01, "eta": 0.01})
+
+study.optimize(objective, n_trials=30)
 
 print("The best parameters:")
 print("Best score:", study.best_trial.value)
 print("Best trial:", study.best_trial.params)
 
-plt.plot(diversities, coherences, "-o")
+plt.scatter(diversities, coherences, c="green")
+
+for i in range(len(diversities) - 1):
+    plt.arrow(diversities[i], coherences[i],
+              diversities[i + 1] - diversities[i],
+              coherences[i + 1] - coherences[i],
+              linestyle="--", color="gray", head_length=0.01)
+
 plt.xlim(0, 1)
-plt.ylim(-1, 1)
+# plt.ylim(-1, 1)
 plt.xlabel("Diversity")
 plt.ylabel("Coherence")
 plt.show()
